47b7edbff42e6acd6b6289dda4ede3b7ea6c60db
Sergey <hardmass3ffect@gmail.com>
2023-04-05 19:09:47 +0300

    add task

---
+++ trainign_java_script/task037.js
@@ -0,0 +1,18 @@
+// function checkScope() {
+//     let j = 'function scope';
+//     if (true) {
+//       i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', j);
+//     return j;
+//   }
+//   console.log(checkScope())
+  const number = 10
+  let number1 = 5
+  console.log(`abnafaa ${number}`)
+  
+function sorted(){
+  a: 10
+  b: 12
+}
---
+++ trainign_java_script/task038.js
@@ -0,0 +1,4 @@
+// const array = [["Peter"],["Jacob", "Alex"],["Max", "John", "Mark"],["Alex", "Jacob", "Mark", "Max"]];
+let num = prompt("Введите число: ");
+// console.log(array[2]);
+console.log(num);
